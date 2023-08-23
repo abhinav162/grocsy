f780db968bd419c170672041d6dceaf59d99d560
Abhinav Aditya <abhinavaditya162@gmail.com>
2023-08-24 00:04:48 +0530

    Minor Update

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,6 @@ const axiosInstance = () => {
     const token = localStorage.getItem("token");
 
     return axios.create({
-        // baseURL: "http://localhost:5000/",
         baseURL: "https://grocsy-backend-8r3g.vercel.app/",
         headers: {
             "Content-Type": "application/json",
